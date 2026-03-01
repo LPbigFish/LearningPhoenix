d792e64cc11406f14a4319a34b67184dd58db28a
LPbigFish <lpbyblock@gmail.com>
2026-03-01 22:29:33 +0100

    vid 17

--- test/support/fixtures/promotions_fixtures.ex
+++ test/support/fixtures/promotions_fixtures.ex
@@ -24,25 +24,4 @@ defmodule Shop.PromotionsFixtures do
 
     promotion
   end
-
-  @doc """
-  Generate a unique promotion code.
-  """
-  def unique_promotion_code, do: "some code#{System.unique_integer([:positive])}"
-
-  @doc """
-  Generate a promotion.
-  """
-  def promotion_fixture(attrs \\ %{}) do
-    {:ok, promotion} =
-      attrs
-      |> Enum.into(%{
-        code: unique_promotion_code(),
-        expires_at: ~U[2026-02-27 10:42:00Z],
-        name: "some name"
-      })
-      |> Shop.Promotions.create_promotion()
-
-    promotion
-  end
 end
